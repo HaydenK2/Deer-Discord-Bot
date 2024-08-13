d21ed564e0628c024636f4bc158b9c0be234ad80
Hayden <haydenk643@gmail.com>
2024-08-13 17:29:53 -0500

    removed node module

--- README.md
+++ README.md
@@ -9,3 +9,12 @@ node.js
 npm install -g nodemon
 or sudo npm install -g nodemon (for mac)
 
+
+
+
+to run:
+
+note check this if nodemon not working: 
+nodemon
+
+
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-const {Client, IntentsBitField} = require('discord.js')
+const {Client, IntentsBitField} = require('discord.js');
 
-token = "dfs"
+// Intents = set of permissions to access set of events
+//  ex. if have Guilds, will know if guild (aka server) is created, etc.
+const client = new Client({
 
-const client = new Client(
-    // Intents = set of permissions to access set of events
-    //  ex. if have Guilds, will know if guild (aka server) is created, etc.
-    intents = [IntentsBitField.Flags.Guilds, 
+    intents: [IntentsBitField.Flags.Guilds, 
                 IntentsBitField.Flags.GuildMembers,
                 IntentsBitField.Flags.GuildMessages,
-                IntentsBitField.Flags.MessageContent]
-);
-client.login(token);
+                IntentsBitField.Flags.MessageContent,
+            ],
+});
+
+client.login("MTI3MzAxNzkzNjMxOTM1MjgzMw.GA4eY4.wKUkTGETkUw51tkh-CvzJrhAUrReieC_IL37dc");
