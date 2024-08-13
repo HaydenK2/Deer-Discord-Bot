e424a7fc1d493225faba6930d536145a4816d1f0
Hayden <haydenk643@gmail.com>
2024-08-13 17:40:58 -0500

    bot can now activate

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ const {Client, IntentsBitField} = require('discord.js');
 //  ex. if have Guilds, will know if guild (aka server) is created, etc.
 const client = new Client({
 
-    intents: [IntentsBitField.Flags.Guilds, 
-                IntentsBitField.Flags.GuildMembers,
-                IntentsBitField.Flags.GuildMessages,
-                IntentsBitField.Flags.MessageContent,
-            ],
+    intents: [
+        IntentsBitField.Flags.Guilds, 
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ],
 });
 
 client.login("MTI3MzAxNzkzNjMxOTM1MjgzMw.GA4eY4.wKUkTGETkUw51tkh-CvzJrhAUrReieC_IL37dc");
